f97d004bde53401bef5cec7912bfbd1b8e261f87
Kelly <kelly.ho@sky.uk>
2019-03-22 09:40:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  drivers.forEach(element => {
+    console.log(element.name)
+  });
+}
+
+function logDriversByHometown(drivers) {
+  drivers.forEach(element => {
+    if(element.hometown == 'Pittsburgh') {
+      console.log(element.name)
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  let newDrivers = [...drivers];
+  newDrivers.sort((a, b) => (a.revenue > b.revenue) ? 1: -1)
+  return newDrivers;
+}
+
+function driversByName(drivers) {
+  let newDrivers = [...drivers];
+  newDrivers.sort((a, b) => (a.name > b.name) ? 1: -1)
+  return newDrivers;
+}
+
+//this compares each entry with the preceeding entry and does this until it has gone through the assigned data. the 1's is about position swapping.
+
+function logDriversByHometown(drivers) {
+  drivers.forEach(element => {
+    if(element.hometown == 'Pittsburgh') {
+      console.log(element.name)
+    }
+  });
+}
+
+function totalRevenue(drivers) {
+  const newDrivers = [...drivers];
+  const result = newDrivers.reduce((sum, { revenue }) => sum + revenue, 0)
+  return result;
+}
+
+function averageRevenue(drivers) {
+  const newDrivers = [...drivers];
+  const result = newDrivers.reduce((sum, { revenue }) => sum + revenue, 0)
+  const avrResult = result/newDrivers.length
+  return avrResult;
+}
+
+//spreading drivers, grabs revenue, sum is the first instance of the array which is being added to avrResults
+
+
+
